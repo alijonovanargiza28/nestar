4802da281ebdf389940b06b8dc1bb55985a80e09
Nargiza <alijonovanargiza38@gmail.com>
2026-08-07 15:10:24 +0500

    feat:build project standarts

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,14 @@
+export enum Message {
+  SOMETHING_WENT_WRONG = "Something went wrong!",
+  NO_DATA_FOUND = "No data is found!",
+  CREATE_FAILED = "Create is failed!",
+  UPDATE_FAILED = "Update is failed!",
+  REMOVE_FAILED = "Upload failed!",
+  BAD_REQUEST = "Bad Request",
+  USED_NICK_PHONE = "you are inserting already used nick or phone!",
+  TOKEN_CREATION_FAILED = "Token creation error",
+  NO_MEMBER_NICK = "No member with that member nick!",
+  BLOCKED_USER = "you have been blocked, contact! admin",
+  WRONG_PASSWORD = "Wrong password insered please try again!",
+  NOT_AUTHENTICATED = "You are not authenticated, Please login first",
+}
