c60d54259965722dad5e845ecd79d8359ef29be5
Nargiza <alijonovanargiza38@gmail.com>
2026-08-07 23:51:14 +0500

    feat:complate

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -3,6 +3,7 @@ import { NestarBatchModule } from './nestar-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarBatchModule);
-  await app.listen(process.env.PORT_BATCH ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3009);
+    console.log("PORT_BATCH:", process.env.PORT_BATCH);
 }
 bootstrap();
