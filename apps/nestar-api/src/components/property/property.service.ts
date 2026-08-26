c031e5c91a5469288d4752ca1f1a99413d25aae1
Nargiza <alijonovanargiza38@gmail.com>
2026-08-26 12:07:48 +0500

    feat:modification of property module

---
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
+import { ViewService } from '../view/view.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class PropertyService {
+  constructor(
+    @InjectModel("Property") private readonly propertyModel: Model<null>,
+  ) {}
+}
