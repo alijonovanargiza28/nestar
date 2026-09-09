c0f1e2c3121ae9e533a0f3a341e31c87ea5e31c2
Nargiza <alijonovanargiza38@gmail.com>
2026-09-09 19:33:27 +0500

    feat:develop job schedule business logic

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,14 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { NestarBatchModule } from './nestar-batch.module';
-import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from 'apps/nestar-api/src/libs/enums/intersepter/loggingIntercepter';
-import { AppModule } from 'apps/nestar-api/src/app.module';
+import { NestFactory } from "@nestjs/core";
+import { NestarBatchModule } from "./nestar-batch.module";
+import { ValidationPipe } from "@nestjs/common";
+import { LoggingInterceptor } from "apps/nestar-api/src/libs/enums/intersepter/loggingIntercepter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  app.useGlobalInterceptors(new LoggingInterceptor())
+  const app = await NestFactory.create(NestarBatchModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
   await app.listen(process.env.PORT_BATCH ?? 3000);
-    console.log("PORT_BATCH:", process.env.PORT_BATCH);
+
+  console.log("PORT_BATCH:", process.env.PORT_BATCH);
 }
+
 bootstrap();
