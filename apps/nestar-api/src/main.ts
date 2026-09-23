62c032b07eb466dd6bd3f265f8e8a87f277303b2
Nargiza <alijonovanargiza38@gmail.com>
2026-09-23 15:56:47 +0500

    fix:photo integration

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -5,17 +5,19 @@ import { LoggingInterceptor } from "./libs/enums/intersepter/loggingIntercepter"
 import { graphqlUploadExpress } from "graphql-upload";
 import * as express from "express";
 import { WsAdapter } from "@nestjs/platform-ws";
+import { join } from "path";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
+  app.use("/uploads", express.static(join(process.cwd(), "uploads")));
   app.enableCors({
     origin: true,
     credentials: true,
   });
 
   app.use(graphqlUploadExpress({ MaxFileSize: 15000000, maxFiles: 10 }));
-  app.use("/uploads", express.static("./uploads"));
+  app.use("/uploads", express.static(".uploads"));
 
   app.useWebSocketAdapter(new WsAdapter(app))
   await app.listen(process.env.PORT_API ?? 3000);
